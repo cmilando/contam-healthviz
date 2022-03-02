edc08c3e3dbe14d2c9d048c1a4fe9762110d1f55
cmilando <44262671+cmilando@users.noreply.github.com>
2022-03-01 19:03:23 -0500

    Update app.R

--- app.R
+++ app.R
@@ -11,6 +11,7 @@ library(jsonlite)
 library(waiter)
 library(jsonlite)
 library(DT)
+library(spsComps)
 
 # =============================================================================
 ui <- fluidPage(
@@ -144,32 +145,34 @@ server <- function(input, output, session) {
   # --------------------------------------
   # f_name data_frame
   all_opts <- reactive({
-    # new json object? kick it off
-    out_dir <- input$out_dir
-    rj <- input$json_obj
-    
-    # 1. load rj
-    files_to_run <- read_json(file.path(out_dir, "01_torun", rj))
-    for(i in seq(files_to_run)) {
-      files_to_run[[i]]$f_name = names(files_to_run)[[i]]
-    }
-    
-    # 2. iterate through, and make the table, include a unique identifier, 
-    #    or just hide the column that is the super long row name
-    #
-    #    This probably involves an Rbind of a `long` format of the `opts` object, 
-    #    followed by a pivot_wider to make the table, and fill with "NA" or similar
-    #    so that different levels of `opts` are allowed
-    #
-    do.call(rbind, lapply(files_to_run, function(x) {
-      x_df <- data.frame(x) %>% 
-        select(starts_with("opts.")) %>%
-        pivot_longer(cols = everything(), names_to = "opt")
-      x_df$name <- x$f_name
-      x_df$opt <- gsub("opts.", "", x_df$opt, fixed = T, perl = F)
-      x_df
-    })) %>% pivot_wider(id_cols = name, names_from = opt)
-    
+    shinyCatch({
+      # new json object? kick it off
+      out_dir <- input$out_dir
+      rj <- input$json_obj
+      if(rj == "") return()
+      
+      # 1. load rj
+      files_to_run <- read_json(file.path(out_dir, "01_torun", rj))
+      for(i in seq(files_to_run)) {
+        files_to_run[[i]]$f_name = names(files_to_run)[[i]]
+      }
+      
+      # 2. iterate through, and make the table, include a unique identifier, 
+      #    or just hide the column that is the super long row name
+      #
+      #    This probably involves an Rbind of a `long` format of the `opts` object, 
+      #    followed by a pivot_wider to make the table, and fill with "NA" or similar
+      #    so that different levels of `opts` are allowed
+      #
+      do.call(rbind, lapply(files_to_run, function(x) {
+        x_df <- data.frame(x) %>% 
+          select(starts_with("opts.")) %>%
+          pivot_longer(cols = everything(), names_to = "opt")
+        x_df$name <- x$f_name
+        x_df$opt <- gsub("opts.", "", x_df$opt, fixed = T, perl = F)
+        x_df
+      })) %>% pivot_wider(id_cols = name, names_from = opt)
+    })
   })
   
   # render the f_name table
@@ -180,90 +183,95 @@ server <- function(input, output, session) {
                    pageLength = 10)
   )
   
+  # ------------------------------------
+  # all db
+  # load in based on
+  contam_data <- reactive({
+    shinyCatch({
+      df <- all_opts()
+      
+      timestamp(suffix = "> get data")
+      
+      s <- input$rj_table_rows_selected
+      
+      xx <- lapply(s, function(i) {
+        
+        x <- df$name[i]
+        
+        x2 <- readRDS(file.path(input$out_dir, "02_outputs", x, 
+                                paste0(x, ".RDS")))
+        x2$sim_name <- x
+        x2
+      })
+      
+      do.call(rbind, xx)
+    })
+  })
+  
   # --------------------------------------
   # for the boxplot
   output$boxplot <- renderPlot({
     
-    # show the spinner
-    w_box$show()
-    
-    df <- all_opts()
-    
-    # load in based on
-    s <- input$rj_table_rows_selected
-
-    contam_data_raw <- lapply(s, function(i) {
-      x <- df$name[i]
-      x2 <- readRDS(file.path(input$out_dir, "02_outputs", x, 
-                              paste0(x, ".RDS")))
-      x2$sim_name <- x
-      x2
+    shinyCatch({
+      
+      # show the spinner
+      w_box$show()
+      
+        # finalize
+      contam_data_x <- contam_data()
+      
+      # hide the spinner
+      w_box$hide()
+      
+      # the plot
+      contam_data_x %>%
+        group_by(Date, Season, ctm, unit, generic_name, name, sim_name) %>%
+        summarize(mean_conc = mean(value_converted),
+                  .groups = "keep") %>%
+        ggplot(.) +
+        geom_boxplot(aes(x = unit, 
+                         y = mean_conc, 
+                         fill = sim_name), outlier.size = 0.25) +
+        theme_bw() +
+        facet_grid(. ~ unit) +
+        theme(legend.position = "bottom",
+              legend.direction = "vertical")
     })
-    
-    # hide the spinner
-    w_box$hide()
-    
-    # finalize
-    contam_data <- do.call(rbind, contam_data_raw)
-    
-    # the plot
-    contam_data %>%
-      group_by(Date, Season, ctm, unit, generic_name, name, sim_name) %>%
-      summarize(mean_conc = mean(value_converted),
-                .groups = "keep") %>%
-      ggplot(.) +
-      geom_boxplot(aes(x = unit, 
-                       y = mean_conc, 
-                       fill = sim_name), outlier.size = 0.25) +
-      theme_bw() +
-      facet_grid(. ~ unit) +
-      theme(legend.position = "bottom",
-            legend.direction = "vertical")
-    
   })
   
   # --------------------------------------
   # for the diurnal
   output$diurnal <- renderPlot({
     
-    # show the spinner
-    w_diurnal$show()
+    shinyCatch({
     
-    df <- all_opts()
+      # show the spinner
+      w_diurnal$show()
+      
+      # finalize
+      contam_data_x <- contam_data()
+      
+      # hide the spinner
+      w_diurnal$hide()
+      
+      # the plot
+      contam_data_x %>%
+        group_by(Hour, ctm, unit, generic_name, name, sim_name) %>%
+        summarize(mean_conc = mean(value_converted),
+                  q25 = quantile(value_converted, probs = .25),
+                  q75 = quantile(value_converted, probs = .75),
+                  .groups = "keep") %>%
+        ggplot(., aes(x = Hour, y = mean_conc, color = sim_name, fill = sim_name,
+                      ymin = q25, ymax = q75)) +
+        geom_ribbon(color = NA, alpha = 0.2) +
+        geom_line() +
+        geom_point() +
+        theme_bw() +
+        facet_grid(. ~ unit) +
+        theme(legend.position = "bottom",
+              legend.direction = "vertical")
     
-    # load in based on
-    s <- input$rj_table_rows_selected
-    
-    contam_data_raw <- lapply(s, function(i) {
-      x <- df$name[i]
-      x2 <- readRDS(file.path(input$out_dir, "02_outputs", x, 
-                              paste0(x, ".RDS")))
-      x2$sim_name <- x
-      x2
     })
-    
-    # hide the spinner
-    w_diurnal$hide()
-    
-    # finalize
-    contam_data <- do.call(rbind, contam_data_raw)
-    
-    # the plot
-    contam_data %>%
-      group_by(Hour, ctm, unit, generic_name, name, sim_name) %>%
-      summarize(mean_conc = mean(value_converted),
-                q25 = quantile(value_converted, probs = .25),
-                q75 = quantile(value_converted, probs = .75),
-                .groups = "keep") %>%
-      ggplot(., aes(x = Hour, y = mean_conc, color = sim_name, fill = sim_name,
-                    ymin = q25, ymax = q75)) +
-      geom_ribbon(color = NA, alpha = 0.2) +
-      geom_line() +
-      geom_point() +
-      theme_bw() +
-      facet_grid(. ~ unit) +
-      theme(legend.position = "bottom",
-            legend.direction = "vertical")
     
   })
   
